41bcfd7d4d114869e46b6c4feac21f64493cdac6
RDG97 <ryangann1997@gmail.com>
2022-11-08 20:21:57 +0000

    sand in the box

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from pprint import pprint
+from .models import polls
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def get_polls(request):
+    data = list(polls.objects.values())
+    return JsonResponse({ 'data': data })
